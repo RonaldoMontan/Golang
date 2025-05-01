3d2e82de01bc4d69250a44f4c875d6ac1eb265db
Ronaldo Montan <ronaldogithub@gmail.com>
2025-05-01 15:07:21 -0300

    feat: Clear terminal to show, check and read letter dedicated

--- jogo_da_forca.go
+++ jogo_da_forca.go
@@ -1,13 +1,17 @@
 package main
 
-//as bibliotecas necessárias
+// bibliotecas necessárias
 import (
 	"fmt"
 	"math/rand"
 	"reflect"
+	"os/exec"
+	"runtime"
+	"os"
+	"bufio"
+	"strings"
 )
 
-// Unica função e principal
 func main() {
 
 	num := rand.Intn(24)
@@ -15,39 +19,96 @@ func main() {
 	adivinha := []string{}
 	palpite := []string{}
 	ja_foram := []string{}
-	letra := ""
-
+	erradas := []string{}
+	// letra := ""
+	
 	for _, valor := range frutas[num] {
 		//fmt.Println(indice, string(valor))
 		adivinha = append(adivinha, string(valor))
 		palpite = append(palpite, "_")
 	}
-
+	
 	fmt.Print("\n\tJogo da forca !\n\n")
 	fmt.Print(">>> Valendo somente nomes de frutas <<<\n\n")
 	fmt.Println("\t", palpite)
-
+	
 	for true {
-		fmt.Print("\n\nInforme uma letra: ")
-		fmt.Scanln(&letra)
+		existeLetra := false
 
+		letra := readLetter()
+
+		if (check(ja_foram, letra) == true){
+			fmt.Println("\n\n\t\t>>Essa letra já foi escolhida !<<")
+			continue
+		}
+		
 		for i := 0; i < len(adivinha); i++ {
 			if adivinha[i] == letra {
 				palpite[i] = letra
+				existeLetra = true
 			}
 		}
+
+		if !existeLetra  {
+			erradas = append(erradas, letra)
+		}
+
 		ja_foram = append(ja_foram, letra)
-		fmt.Println("\t ._______")
+		show(palpite, ja_foram, erradas)
+
+		if reflect.DeepEqual(adivinha, palpite) == true {
+			fmt.Println("\n\n\t\t>>Parabéns, Você acertou !!<<")
+			break
+		} else if len(erradas) == 5 {
+			end(adivinha)
+			break
+		}
+	}
+}
+
+func readLetter() string {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("\nInforme uma letra: ") 
+    input = strings.TrimSpace(input)             // Remove espaços e \n
+    return strings.ToLower(input)                // Padroniza para minúscula
+}
+
+func check(ja_foram []string, letra string) bool {	
+	for _, l := range ja_foram {
+		if l == letra {
+			return true
+		}
+	}
+	return false
+}
+
+func show(palpite, ja_foram , erradas []string) {
+	clearTerminal()
+	fmt.Print("\n\tJogo da forca !\n\n")
+	fmt.Print(">>> Valendo somente nomes de frutas <<<\n\n")
+	fmt.Println("\t ._______")
 		fmt.Println("\t |/      *")
 		fmt.Println("\t |")
 		fmt.Println("\t |")
 		fmt.Println("\t", palpite)
 		fmt.Println("\n\n>Letras escolhidas<", ja_foram)
+		fmt.Println("\n\n>Letras erradas<", erradas)
+}
 
-		if reflect.DeepEqual(adivinha, palpite) == true {
-			break
-		}
+func end(adivinha []string) {
+	fmt.Println("\t\tTentativas excedidas !")
+	fmt.Println("\t\t", adivinha)
+}
+
+func clearTerminal() {
+
+	var cmd *exec.Cmd
+	
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
 	}
-	fmt.Println("\n\n\t\t>>Parabéns, Você acertou !!<<")
-
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
